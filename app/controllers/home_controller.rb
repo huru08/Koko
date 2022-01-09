746a1864e249bdcab92ee3da317377a8d98eb004
Kawata <1997tatuya@gmail.com>
2022-01-10 00:37:51 +0900

    占いエラー１

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -19,7 +19,7 @@ def fortune
   {title: "雲を見つめよ", body: "上を見て歩きなさい、", under:"立ちはだかる壁を越える手助けとなるでしょう。",pic: 2},
   {title: "友を観察せよ", body: "近き者に目を向けよ", under:"懐かしい記憶があなたを少し前へ導く事でしょう。",pic: 3},
   {title: "家族と向き合え", body: "気をつかうな、", under:"あなたの味方が最も近くにいることに気づくことでしょう。",pic: 4},
-  {title: "よく笑え", body: "笑う角には福来たる、", under:"上を向くものには待人来たる。",pic: 5},
+  {title: "よく笑え", body: "笑う角には福来たる、", under:"上を向くものには待人来たる。",pic: 5}
 ]
 @picture = @telling[@randam][:pic] + @telling[@randam2][:pic]
 
