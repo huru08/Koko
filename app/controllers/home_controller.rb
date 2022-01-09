840e8e94ba6878dc38e6bc92546039d016b78019
Kawata <1997tatuya@gmail.com>
2022-01-10 02:39:40 +0900

    占いエラー４

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -12,13 +12,21 @@ def koko
 end
 
 def fortune
-  @randam = SecureRandom.random_number(5)
-  @randam2 = SecureRandom.random_number(5)
-  @telling = [{title: "晴天の下へ", body: "一番明るい空の下で深呼吸をせよ", under:"幸運があなたを迎えにくることでしょう。", pic: 1},
-  {title: "雲を見つめよ", body: "上を見て歩きなさい、", under:"立ちはだかる壁を越える手助けとなるでしょう。", pic: 2},
-  {title: "友を観察せよ", body: "近き者に目を向けよ", under:"懐かしい記憶があなたを少し前へ導く事でしょう。", pic: 3},
-  {title: "家族と向き合え", body: "気をつかうな、", under:"あなたの味方が最も近くにいることに気づくことでしょう。", pic: 4},
-  {title: "よく笑え", body: "笑う角には福来たる、", under:"上を向くものには待人来たる。", pic: 5}]
+  @telling = [{title: "晴天の下へ", body: "一番明るい空の下で深呼吸をせよ", under:"幸運があなたを迎えにくることでしょう。", pic: 0},
+  {title: "雲を見つめよ", body: "上を見て歩きなさい、", under:"立ちはだかる壁を越える手助けとなるでしょう。", pic: 1},
+  {title: "友を観察せよ", body: "近き者に目を向けよ", under:"懐かしい記憶があなたを少し前へ導く事でしょう。", pic: 2},
+  {title: "家族と向き合え", body: "気をつかうな、", under:"あなたの味方が最も近くにいることに気づくことでしょう。", pic: 3},
+  {title: "よく笑え", body: "笑う角には福来たる、", under:"上を向くものには待人来たる。", pic: 4}]
+
+  @telling2 = [{title: "晴天の下へ", body: "一番明るい空の下で深呼吸をせよ", under:"幸運があなたを迎えにくることでしょう。", pic: 0},
+  {title: "雲を見つめよ", body: "上を見て歩きなさい、", under:"立ちはだかる壁を越える手助けとなるでしょう。", pic: 1},
+  {title: "友を観察せよ", body: "近き者に目を向けよ", under:"懐かしい記憶があなたを少し前へ導く事でしょう。", pic: 2},
+  {title: "家族と向き合え", body: "気をつかうな、", under:"あなたの味方が最も近くにいることに気づくことでしょう。", pic: 3},
+  {title: "よく笑え", body: "笑う角には福来たる、", under:"上を向くものには待人来たる。", pic: 4}]
+
+@randam = @telling.sample[:pic]
+@randam2 = @telling2.sample[:pic]
+
   @picture = @randam + @randam2
 
 end
